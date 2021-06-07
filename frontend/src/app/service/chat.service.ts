89d9132794925dfd42d852f144aac971b1e75bbc
刘亦奇 <liuyiqi1999@163.com>
2021-06-07 17:36:35 +0800

    被at时会收到提示

--- frontend/src/app/service/chat.service.ts
+++ frontend/src/app/service/chat.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable, Subject } from "rxjs";
 import { WebsocketService } from "./websocket.service";
+import { NzMessageService } from 'ng-zorro-antd/message';
 import "rxjs/add/operator/map";
 
 const CHAT_URL_BASE = 'ws://localhost:8085/chat';
@@ -9,7 +10,7 @@ const CHAT_URL_BASE = 'ws://localhost:8085/chat';
 export class ChatService {
   public messages: Subject<any>;
 
-  constructor(wsService: WebsocketService) {
+  constructor(wsService: WebsocketService, private message: NzMessageService) {
     this.messages = <Subject<any>>(
       wsService.connect(CHAT_URL_BASE+`/${localStorage.getItem("token")}`).map((response: MessageEvent): any => {
         let data = JSON.parse(response.data);
@@ -18,6 +19,9 @@ export class ChatService {
           data.type="userList";
           return data;
         }else if(data.message){
+          if(new Set(data.at).has(localStorage.getItem("username"))){
+            this.message.info(`${data.author}: ${data.message}`);
+          }
           data.type="message";
           return data;
         }
