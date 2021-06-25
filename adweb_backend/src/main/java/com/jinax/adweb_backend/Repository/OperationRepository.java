c1fe4caaaa712c5d266a6a90d23a82b71f73ca0b
jinaxc <1005454946@qq.com>
2021-06-26 03:12:38 +0800

    add @modifying

--- adweb_backend/src/main/java/com/jinax/adweb_backend/Repository/OperationRepository.java
+++ adweb_backend/src/main/java/com/jinax/adweb_backend/Repository/OperationRepository.java
@@ -2,6 +2,7 @@ package com.jinax.adweb_backend.Repository;
 
 import com.jinax.adweb_backend.Entity.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -15,7 +16,7 @@ import java.util.Map;
 public interface OperationRepository extends JpaRepository<Operation,Integer> {
 //    @Query(value = "select 'from',`to`,plate_size,hh.id history_id from operation left join hanoi_history hh on operation.id = hh.operation_id left join user u on operation.user_id = u.id where u.username = ?1 and game_id = ?2",nativeQuery = true)
 //    public List<Map<String,Integer>> getAllOperationsByUserIdAndGameId(String username, Integer gameId);
-
+    @Modifying
     @Query(value = "insert into operation (user_id, `from`, `to`, plate_size) VALUES (':#{#o.userId}',':#{#o.from}',':#{#o.to}',':#{#o.plateSize}')",nativeQuery = true)
     public Operation insertOperation(Operation o);
 }
