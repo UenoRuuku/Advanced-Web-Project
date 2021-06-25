531eb4d6bccdba1f02216c8c5652929903a0874f
jinaxc <1005454946@qq.com>
2021-06-26 03:11:07 +0800

    fix bug in sql

--- adweb_backend/src/main/java/com/jinax/adweb_backend/Repository/OperationRepository.java
+++ adweb_backend/src/main/java/com/jinax/adweb_backend/Repository/OperationRepository.java
@@ -16,6 +16,6 @@ public interface OperationRepository extends JpaRepository<Operation,Integer> {
 //    @Query(value = "select 'from',`to`,plate_size,hh.id history_id from operation left join hanoi_history hh on operation.id = hh.operation_id left join user u on operation.user_id = u.id where u.username = ?1 and game_id = ?2",nativeQuery = true)
 //    public List<Map<String,Integer>> getAllOperationsByUserIdAndGameId(String username, Integer gameId);
 
-    @Query(value = "insert into operation (user_id, `from`, `to`, plate_size) VALUES (:#{#o.userId},:#{#o.from},:#{#o.to},:#{#o.plateSize})",nativeQuery = true)
+    @Query(value = "insert into operation (user_id, `from`, `to`, plate_size) VALUES (':#{#o.userId}',':#{#o.from}',':#{#o.to}',':#{#o.plateSize}')",nativeQuery = true)
     public Operation insertOperation(Operation o);
 }
